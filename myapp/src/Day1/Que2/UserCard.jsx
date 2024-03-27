11136d290a69481827c4b7625187dacfbbf5a97f
anantmishra-successive <anant.mishra@successive.tech>
2024-03-27 16:25:32 +0530

    add:ques

--- myapp/src/Day1/Que2/UserCard.jsx
+++ myapp/src/Day1/Que2/UserCard.jsx
@@ -1,3 +1,8 @@
+// 2.Create a functional component named UserCard that accepts the following props: name, email, and avatarURL.
+// Use the props to display the user's name, email, and an image (avatar) in the UserCard component.
+// Create multiple instances of the UserCard component with different user data.
+
+
 import React from 'react'
 import './UserCard.css'
 function UserCard(props) {
--- myapp/src/Day1/Que3/Weather.jsx
+++ myapp/src/Day1/Que3/Weather.jsx
@@ -1,3 +1,8 @@
+// 3.Create a functional component named Weather that accepts a prop called temperature (a number).
+// Display a message like "It's sunny today!" if the temperature is above 25°C and "It's cold today!" if the temperature is below 10°C.
+// Import and render the Weather component in the App component with different temperature values.
+
+
 import React from 'react'
 
 function Weather(props) {
